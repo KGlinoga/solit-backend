a40c04ce6723dcfc37f5e68aacb84f7dd9304198
asiya <asiyaahmed33@yahoo.com>
2022-09-01 14:22:23 -0700

    fixed-up book routes and review averages

--- controllers/api/bookRoutes.js
+++ controllers/api/bookRoutes.js
@@ -1,89 +1,65 @@
 const router = require('express').Router();
+const sequelize = require('sequelize');
 const { Book, Review } = require('../../models');
 
 router.get("/", (req, res) => {
-    Book.findAll({
-      include: [Review],
-    }).then((results) => {
-        
-        const updatedResults = results.map((result) => {
-        const avg_plotRating = (result.Reviews.reduce( (a,b)=> {
-                return a.plot_rating + b.plot_rating
-               }) ) / result.Reviews.length;
+  Book.findAll({
+    include: [{
+      model: Review,
+      attributes: [
+        [sequelize.fn("avg",sequelize.col("plot_rating")), "plotAvg"],
+        [sequelize.fn("avg",sequelize.col("character_rating")), "characterAvg"],
+        [sequelize.fn("avg",sequelize.col("accessibility_rating")), "accessibilityAvg"],
+        [sequelize.fn("avg",sequelize.col("pacing_rating")), "pacingAvg"],
+      ]}],
+  }).then((results) => {
 
-        const avg_characterRating = (result.Reviews.reduce( (a,b)=> {
-                return a.character_rating + b.character_rating
-               }) ) / result.Reviews.length;
-
-        const avg_accessibilityRating = (result.Reviews.reduce( (a,b)=> {
-                return a.accessibility_rating + b.accessibility_rating
-               }) ) / result.Reviews.length;
-
-        const avg_pacingRating = (result.Reviews.reduce( (a,b)=> {
-                return a.pacing_rating + b.pacing_rating
-               }) ) / result.Reviews.length;
-
-            return  {
-                id: result.id,
-                book_title: result.book_title,
-                author: result.author,
-                cover: result.cover,
-                description: result.description,
-                ISBN: result.ISBN,
-                avg_plotRating: avg_plotRating,
-                avg_characterRating: avg_characterRating,
-                avg_accessibilityRating: avg_accessibilityRating,
-                avg_pacingRating: avg_pacingRating,
-                Reviews: result.Reviews
-            };
-        })
-       
-      res.json(updatedResults);
-    });
+    res.json(results);
   });
-  
-  router.get("/:id", (req, res) => {
-    Book.findOne({
+});
+
+router.get("/:id", (req, res) => {
+  Book.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Review],
+  }).then((result) => {
+    res.json(result);
+  });
+});
+
+router.post("/", (req, res) => {
+  Book.create(req.body).then((result) => {
+    res.json(result);
+  });
+});
+
+router.put("/:id", (req, res) => {
+  Book.update(
+    req.body,
+    {
       where: {
         id: req.params.id,
       },
-      include: [Review],
-    }).then((result) => {
-      res.json(result);
-    });
-  });
-  
-  router.post("/", (req, res) => {
-    Book.create(req.body).then((result) => {
-      res.json(result);
-    });
-  });
-  
-  router.put("/:id", (req, res) => {
-    Book.update(
-      req.body,
-      {
-        where: {
-          id: req.params.id,
-        },
-      },
-      {
-        new: true,
-      }
-    )
+    },
+    {
+      new: true,
+    }
+  )
     .then((result) => {
       res.json(result)
     });
+});
+
+router.delete("/:id", (req, res) => {
+  Book.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((result) => {
+    res.json(result);
   });
-  
-  router.delete("/:id", (req, res) => {
-    Book.destroy({
-      where: {
-        id: req.params.id,
-      },
-    }).then((result) => {
-      res.json(result);
-    });
-  });
-  
-  module.exports = router;
+});
+
+module.exports = router;
--- models/Book.js
+++ models/Book.js
@@ -17,30 +17,10 @@ Book.init({
         type: DataTypes.STRING,
         allowNull:false
     },
-    description: {
-        type: DataTypes.STRING,
-        allowNull:false
-    },
     ISBN: {
         type: DataTypes.INTEGER,
         allowNull:false
-    },
-    // avg_plotRating: {
-    //     type: DataTypes.INTEGER,
-    //     allowNull:false
-    // },
-    // avg_characterRating: {
-    //     type: DataTypes.INTEGER,
-    //     allowNull:false
-    // },
-    // avg_accessRating: {
-    //     type: DataTypes.INTEGER,
-    //     allowNull:false
-    // },
-    // avg_paceRating: {
-    //     type: DataTypes.INTEGER,
-    //     allowNull:false
-    // }
+    }
 },{
     sequelize,
     freezeTableName: true,
